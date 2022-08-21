87b5cea747f05a8a6176ec0410db548491bd4439
Eusebio Simango <eusebiosimango14@gmail.com>
2022-08-22 00:24:06 +0200

    Clean...

--- calculator/app.js
+++ calculator/app.js
@@ -7,9 +7,6 @@ buttons.map(button => {
     if (event.target.classList.contains("number") || event.target.classList.contains("operation") || event.target.classList.contains("parentheses"))
       return renderNumber(event.target)
     
-    else if (event.target.classList.contains("operation"))
-      return pushToListOfNumbers(event.target)
-    
     else if (event.target.classList.contains("equals"))
       return calculate()
 
@@ -27,46 +24,16 @@ const renderNumber = (number) => {
 
   input.value = `${input.value}` + number.getAttribute('data-value')
 
-  var a = input.value
 }
 
-const pushToListOfNumbers = (number) => {
-  const input = window.document.getElementsByClassName('input')[0]
-  // listOfNumbers.push(input.value)
-}
 
 const calculate = () => {
-  const inputValue =[...window.document.getElementsByClassName('input')[0].value]
-  console.log(inputValue)
-  const list = []
-  var acc = ''
-  var newList = []
-  inputValue.map(digit => {
-    console.log(String(Number(digit)))
-    if (String(Number(digit)) != 'NaN') list.push(Number(digit))
-    else list.push(digit)
-  })
+  const inputValue = window.document.getElementsByClassName('input')[0].value
+  const resultDisplay = window.document.getElementsByClassName('result')[0]
+  const result = eval(inputValue)
 
-  list.map((num, i) => {
-    console.log('len -', acc.length)
-    if (acc.length > 0) {
-      acc += String(num)
-      console.log('acc =', acc)
-      return
-    }
-    console.log('now: ', i, '-', acc)
-    if (num = '+')
-      if (list[i+1])
-        console.log('adding', list[i+1])
-        newList.push(acc)
-        newList.push(num)
-        return acc = ''
-        
-    
-    
-  })
-  console.log(newList)
-  console.log('result: ', acc)
+  resultDisplay.innerHTML = result
+
 }
 
 const cleanInput = () => {
